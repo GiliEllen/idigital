2dba4391f8b79e743e35b8c829b4885c536f0e22
Gili <gilimena95@gmail.com>
2023-06-12 10:01:52 +0300

    small changes

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,7 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
  
 dotenv.config();
@@ -18,6 +18,7 @@ import productsRoutes from "./API/products/productsRoutes";
 app.use("/products", productsRoutes);
 
 import usersRoutes from "./API/users/usersRoutes";
+import { corsOptions } from "./config/corsOptions";
 app.use("/users", usersRoutes);
 
 app.get("/test", (req, res) => {
