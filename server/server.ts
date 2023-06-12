c035c9ba1ee52b879a4c011c234314e4cca5be7f
Gili <gilimena95@gmail.com>
2023-06-12 10:40:08 +0300

    Revert "small changes"
    This reverts commit 2dba4391f8b79e743e35b8c829b4885c536f0e22.

--- server/server.ts
+++ server/server.ts
@@ -9,7 +9,7 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
  
 dotenv.config();
@@ -18,7 +18,6 @@ import productsRoutes from "./API/products/productsRoutes";
 app.use("/products", productsRoutes);
 
 import usersRoutes from "./API/users/usersRoutes";
-import { corsOptions } from "./config/corsOptions";
 app.use("/users", usersRoutes);
 
 app.get("/test", (req, res) => {
