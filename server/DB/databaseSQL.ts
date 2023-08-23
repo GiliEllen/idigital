3d8620d7462d5388625127064c24104ff9cede8a
Gili <gilimena95@gmail.com>
2023-08-23 14:16:13 +0300

    Update databaseSQL.ts

--- server/DB/databaseSQL.ts
+++ server/DB/databaseSQL.ts
@@ -1,33 +1,39 @@
 import mysql from "mysql";
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-const SQL_PASSWORD = process.env.JWT_SECRET
- 
+const SQL_PASSWORD = process.env.JWT_SECRET;
+
+//old connection
+// export const connection = mysql.createConnection({
+//     host: "sql7.freemysqlhosting.net",
+//     user: "sql7616400",
+//     password: "IQY4CeQnYn",
+//     database: "sql7616400",
+//     port: 3306
+// });
 export const connection = mysql.createConnection({
-    host: "sql7.freemysqlhosting.net",
-    user: "sql7616400",
-    password: "IQY4CeQnYn",
-    database: "sql7616400",
-    port: 3306
+  host: "sql7.freemysqlhosting.net",
+  user: "sql7641817",
+  password: "aeMCTh7KPZ",
+  database: "sql7616400",
+  port: 3306,
 });
 var del = connection._protocol._delegateError;
-connection._protocol._delegateError = function(err, sequence){
+connection._protocol._delegateError = function (err, sequence) {
   if (err.fatal) {
-    console.trace('fatal error: ' + err.message);
+    console.trace("fatal error: " + err.message);
   }
   return del.call(this, err, sequence);
 };
 
 connection.connect((error) => {
-    try {
-        if(error) throw error;
-        console.log("Connected to MySQL");
-        
-    } catch (error) {
-        console.error(error);
-        console.log(error.fatal)
-        if (error.fatal) console.log("FATAL")
-    }
+  try {
+    if (error) throw error;
+    console.log("Connected to MySQL");
+  } catch (error) {
+    console.error(error);
+    console.log(error.fatal);
+    if (error.fatal) console.log("FATAL");
+  }
 });
-
